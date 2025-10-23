ac240cd5feb08747831938a7fe6784b38055f4b0
Amin Espinoza <amin.espinoza@outlook.com>
2025-10-23 17:56:55 -0600

    My first leet code

---
+++ CSharp/Basicos/3461LeetCode/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+//https://leetcode.com/problems/check-if-digits-are-equal-in-string-after-operations-i/description/
+bool HasSameDigits(string s)
+{
+    while (s.Length > 2)
+    {
+        var newDigits = new StringBuilder();
+        for (int i = 0; i < s.Length - 1; i++)
+        {
+            int sum = (s[i] - '0') + (s[i + 1] - '0');
+            newDigits.Append(sum % 10);
+        }
+        s = newDigits.ToString();
+    }
+    return s[0] == s[1];
+}
+
+// Ejemplo de uso 1
+string input = "3902";
+bool result = HasSameDigits(input);
+Console.WriteLine(result);
+
+// Ejemplo de uso 2
+string input2 = "34789";
+bool result2 = HasSameDigits(input2);
+Console.WriteLine(result2);
+
+
